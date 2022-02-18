8f1ff4ff3c99245716337097baddad1934c01a34
wsrtka <witold.serwatka@pm.me>
2022-02-18 17:16:52 +0100

    add visualization function

--- training.py
+++ training.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 from keras import Input, Model
 from keras.optimizers import Adam
@@ -33,8 +34,20 @@ def get_combined():
     return combined
 
 
-def plot_generated_images(epoch, gen):
-    ...
+def plot_generated_images(epoch, generator, examples=100, dim=(10, 10), figsize=(10, 10)):
+    noise = np.random.normal(0, 1, size=[examples, dim])
+    generated_images = generator.predict(noise)
+    generated_images = generated_images.reshape(examples, 28, 28)
+
+    plt.figure(figsize=figsize)
+
+    for i in range(generated_images.shape[0]):
+        plt.subplot(dim[0], dim[1], i+1)
+        plt.imshow(generated_images[i], interpolation='nearest', cmap='gray_r')
+        plt.axis('off')
+
+    plt.tight_layout()
+    plt.savefig(f'results/gan_generated_image_epoch_{epoch}.png')
 
 
 def train(epochs: int, X_train, generator: Model, discriminator: Model, combined: Model, batch_size=128, save_interval=50):
